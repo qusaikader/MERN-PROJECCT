05c5955a9ed506cedd46a70493ef9e4ac3382e70
Nirav Gujarathi <82361028+Netizen02@users.noreply.github.com>
2022-12-05 15:19:32 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css"
+import React from "react";
 import Auth from "./pages/Auth/Auth";
-import Home from "./pages/home/Home";
-import Profile from "./pages/Profile/Profile";
+import tweetPage from "./pages/tweetPage/tweetPage";
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-        <div className="blur" style={{top: '-18%', right: '0'}}></div>
-        <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
-        {/* <Home/> */}
-        <Profile/>
-        {/* <Auth/> */}
+      <div className="blur" style={{top: '-18%', right: '0'}}></div>
+      <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
+      <Auth/>
+      {/* <tweetPage/> */}
     </div>
   );
 }
